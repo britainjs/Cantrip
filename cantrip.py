ae140cf7d43c5825146f46cfd0ede6db890600ef
britainjs <bsouthwi@lion.lmu.edu>
2014-07-29 02:47:31 -0700

    Character cap implemented with front-end feedback.

--- cantrip.py
+++ cantrip.py
@@ -1,11 +1,23 @@
 """ A simple web application that generates a list of anagrams from an input string"""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 app = Flask(__name__)
 
-@app.route('/')
-def scramble():
+@app.route('/', methods=['GET', 'POST'])
+def submit():
+    error = None
+    if request.method == 'POST':
+        word = request.form['input_word']
+        if len(word) > 6: 
+            error = "Only words of 6 letters or less accepted"
+            return render_template('layout.html', error=error)
+        else:
+            scramble(word)
     return render_template('layout.html')
+
+def scramble(word):
+    anagrams = set()
+    return render_template('layout.html', anagrams=anagrams)
 
 if __name__ == '__main__':
     app.run()
