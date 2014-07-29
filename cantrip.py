cc5a31cadb8cadb852fc8215bf6cfc4c8526634f
britainjs <bsouthwi@lion.lmu.edu>
2014-07-29 01:17:23 -0700

    Added some placeholder text for the '/' endpoint.

--- cantrip.py
+++ cantrip.py
@@ -1,5 +1,11 @@
+""" A simple web application that generates a list of anagrams from an input string"""
+
 from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def scramble():
+    return "Page Under Construction."
+
+if __name__ == '__main__':
+    app.run()
