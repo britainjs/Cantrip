bd3e536430e2848acec92d398ad0c45f19710121
britainjs <bsouthwi@lion.lmu.edu>
2014-07-29 03:08:25 -0700

    Anagram functionality working.
    Page needs styling and tests now.

--- cantrip.py
+++ cantrip.py
@@ -1,6 +1,13 @@
-""" A simple web application that generates a list of anagrams from an input string"""
+""" 
+    A simple web application that generates a list of anagrams from an input string.
+    
+    Thanks to http://stackoverflow.com/questions/8306654/finding-all-possible-permutations-of-a-given-string-in-python
+    for helping consolidate the permutation function.
+"""
 
 from flask import Flask, request, render_template, flash
+from itertools import permutations
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,12 +19,9 @@ def submit():
             error = "Only words of 6 letters or less accepted"
             return render_template('layout.html', error=error)
         else:
-            scramble(word)
+            anagrams = [''.join(result) for result in permutations(word)]
+            return render_template('layout.html', anagrams=anagrams)
     return render_template('layout.html')
-
-def scramble(word):
-    anagrams = set()
-    return render_template('layout.html', anagrams=anagrams)
 
 if __name__ == '__main__':
     app.run()
